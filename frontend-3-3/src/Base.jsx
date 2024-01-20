ef7f8662d2fc3ba04a7081c544160db2a5d36dc2
abdurahman <atger@gmail.com>
2024-01-20 15:53:33 +0300

    frontend-3-3

---
+++ frontend-3-3/src/Base.jsx
@@ -0,0 +1,10 @@
+import logo from "./assets/i.webp";
+function Base() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <img src={logo} alt="111" />
+    </div>
+  );
+}
+export default Base;
